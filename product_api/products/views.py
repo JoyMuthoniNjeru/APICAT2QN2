066307f0910435662749dbaffeed3feb4f1bae37
JoyMuthoniNjeru <joy.njeru23@strathmore.edu>
2024-11-26 14:18:00 +0300

    handling requests and responses

--- product_api/products/views.py
+++ product_api/products/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -9,13 +6,26 @@ from .serializers import ProductSerializer
 
 class ProductList(APIView):
     def get(self, request):
+        # Retrieve all products from the database
         products = Product.objects.all()
+
+        # Serialize the data
         serializer = ProductSerializer(products, many=True)
+
+        # Return the serialized data as JSON
         return Response(serializer.data)
 
     def post(self, request):
+        # Deserialize the incoming data
         serializer = ProductSerializer(data=request.data)
+
+        # Check if the data is valid
         if serializer.is_valid():
+            # If valid, save the product to the database
             serializer.save()
+
+            # Respond with the serialized data and 201 Created status code
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            # If the data is invalid, return errors and 400 Bad Request status code
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
